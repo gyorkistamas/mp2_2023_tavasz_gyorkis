ce5d02160d3bc9ded6cd471740f43e2b7673a8be
Tamás Györkis <tamasgyorkis@gmail.com>
2023-03-01 14:52:37 +0100

    ELlső óra

---
+++ 1_Diakok/Diak.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Diakok
+{
+    class Diak
+    {
+        public string nev;
+
+        public Lakcim lakcim;
+
+        public Tagozat tagozat;
+
+        public DateTime szuletesNap;
+
+        public int kezdesEve;
+    }
+}
---
+++ 1_Diakok/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _1_Diakok
+{
+    internal class Program
+    {
+
+        public static void Kiir(Diak diak)
+        {
+            Console.WriteLine($"Név: {diak.nev}");
+            Console.WriteLine("Születés nap: {0}", diak.szuletesNap);
+            Console.WriteLine("Lakcím: {0}, {1}, {2}", diak.lakcim.iranyitoszam, diak.lakcim.utcaNev, diak.lakcim.hazSzam);
+            Console.WriteLine("Tagozat: {0}", diak.tagozat);
+            Console.WriteLine("Kezdés éve: {0}", diak.kezdesEve);
+        }
+
+        static void Main(string[] args)
+        {
+            Diak tamas = new Diak();
+
+            tamas.nev = "Györkis Tamás";
+
+            tamas.lakcim = new Lakcim();
+            tamas.lakcim.iranyitoszam = 3000;
+            tamas.lakcim.utcaNev = "asdasdsadas utca";
+            tamas.lakcim.hazSzam = 20;
+
+            tamas.tagozat = Tagozat.NAPPALI;
+
+            tamas.szuletesNap = new DateTime(2001, 4, 30);
+
+            tamas.kezdesEve = 2021;
+
+            Kiir(tamas);
+
+            Console.ReadLine();
+        }
+    }
+}
